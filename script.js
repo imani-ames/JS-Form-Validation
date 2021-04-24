a6d0e61b77b40b685a091b35d954bd51b00c92a0
Imani <imaniames3@gmail.com>
2021-04-24 17:24:28 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if(username.value === '' || username.value == null) {
+    messages.push('Username is required')
+  }
+  
+  if(messages.length > 0) {
+    e.preventDefault() 
+    errorElement.innerText = messages.join(' ,')
+  }
+})
